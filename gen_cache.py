9fe717eaa52a79013c735ba8e872c14d18f6eec7
ciubecca <lorenzovitale89@gmail.com>
2021-11-06 23:21:48 +0000

    skipping already done

--- gen_cache.py
+++ gen_cache.py
@@ -1,3 +1,5 @@
+import os.path
+
 from pytorch3dunet.unet3d.utils import get_logger, Phase
 from pytorch3dunet.unet3d.config import parse_args
 from pytorch3dunet.datasets.utils_pdb import PdbDataHandler
@@ -22,16 +24,20 @@ if __name__ == '__main__':
     def makeRawsLabels(x: PdbDataHandler):
         name = x.name
         cache_folder = x.cache_folder
-        try:
-            x.getRawsLabels(features=features, grid_size=run_config.loaders_config.grid_size,
-                            ligand_mask_radius=pdb_config.ligand_mask_radius)
-            Path(f'{cache_folder}/done').touch()
 
-        except Exception as e:
-            if run_config.fail_on_error:
-                raise e
-            logger.error(f'Generation of grids from {name} failed. Recording failure and continuing', exc_info=True)
-            Path(f'{cache_folder}/failed').touch()
+        if os.path.exists(Path(f'{cache_folder}/done')):
+            logger.info(f'Grid {name} already existed. Skipping.')
+        else:
+            try:
+                x.getRawsLabels(features=features, grid_size=run_config.loaders_config.grid_size,
+                                ligand_mask_radius=pdb_config.ligand_mask_radius)
+                Path(f'{cache_folder}/done').touch()
+                logger.info(f'Computation of grid {name} completed')
+            except Exception as e:
+                if run_config.fail_on_error:
+                    raise e
+                logger.error(f'Generation of grids from {name} failed. Recording failure and continuing', exc_info=True)
+                Path(f'{cache_folder}/failed').touch()
 
     PdbDataHandler.map_datasets(loaders_config=run_config.loaders_config, pdb_workers=run_config.pdb_workers,
                                 features_config=features_config, transformer_config=config['transformer'],
